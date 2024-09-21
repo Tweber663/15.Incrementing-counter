fe15af4496777fcc41e37f6f0558c6017b9a2258
Tweber663 <tweber.1995@hotmail.com>
2024-09-21 10:50:21 +0200

    counter working

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,16 @@
-import Stopwatch from './StopwatchWithResults.js';
-new Stopwatch(document.querySelector('.stopwatch'));
+"use strict";
+const counter = document.querySelectorAll('.counter');
+counter.forEach((counter) => {
+    counter.innerText = "0";
+    const updateCounter = () => {
+        const targetAmount = Number(counter.getAttribute('data-target'));
+        let currentAmount = Number(counter.innerText);
+        const increasedBy = targetAmount / 100;
+        if (currentAmount < targetAmount) {
+            counter.innerText = `${Math.ceil(currentAmount + increasedBy)}`;
+            setTimeout(updateCounter, 10);
+        }
+    };
+    updateCounter();
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
-import Stopwatch from './StopwatchWithResults.js'
+const counter = document.querySelectorAll('.counter') as NodeListOf<HTMLDivElement>;
 
-new Stopwatch(document.querySelector('.stopwatch'))
+counter.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+        const targetAmount = Number(counter.getAttribute('data-target')); 
+        let currentAmount = Number(counter.innerText); 
+        const increasedBy = targetAmount / 100;
+        if (currentAmount < targetAmount) {
+             counter.innerText = `${Math.ceil(currentAmount + increasedBy)}`
+             setTimeout(updateCounter, 10);
+            }
+    }
+    updateCounter(); 
+})
